cdd523878ad60b8eb3654fa104e2c33412f53ed0
Marcin <Marcin@DESKTOP-33601SU>
2022-02-24 14:56:33 +0100

    Add project files.

---
+++ TI-Sklep/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TI_Sklep.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
