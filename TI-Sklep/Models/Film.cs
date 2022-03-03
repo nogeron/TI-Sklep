e5ee06653f2f72058d8c0ded6ab4e773104a5b4f
Marcin Pezda <80229@student.ukw.edu.pl>
2022-03-03 14:52:13 +0100

    Dodanie wstępnych klas

---
+++ TI-Sklep/Models/Film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TI_Sklep.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        public string Tytul { get; set; }
+
+        public string Rezyser { get; set; }
+
+        public string Opis { get; set; }
+
+        public decimal Cena { get; set; }
+
+        public DateTime DataProdukcji { get; set; }
+    }
+}
---
+++ TI-Sklep/Models/Kategoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TI_Sklep.Models
+{
+    public class Kategoria
+    {
+        public int Id { get; set; }
+
+        public int Nazwa { get; set; }
+        public int Opis { get; set; }
+    }
+}
